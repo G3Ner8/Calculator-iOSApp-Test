81f777971c0fb2eda09eb89ea5563ad63d0ff3c7
 ​Chettawat Phosri <Shinosuke@Chettawats-Mac.local>
2014-07-19 20:00:23 -0700

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by  ​Chettawat Phosri on 19/7/57.
+//  Copyright (c) พ.ศ. 2557 Chettawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by  ​Chettawat Phosri on 19/7/57.
+//  Copyright (c) พ.ศ. 2557 Chettawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
